afa30e219b07d30fe21fd8250298b66c5b19aa48
sai varshitha <velma_saivarshitha@epam.com>
2025-03-11 18:11:29 +0530

    dummy commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
  class Main {
     public static void main(String[] args) {
-        System.out.println("This is main");
+        feature2 fe =  new feature2();
+        fe.feature();
     }
 }
---
+++ src/main/java/org/example/feature2.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class feature2 {
+    public void feature(){
+        System.out.println("This is feature 2");
+    }
+}
